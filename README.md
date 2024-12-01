1028e57b193fdcbd5e3acab501f60f2a286fe51a
velmanatov <velmanatov@hotmail.com>
2024-12-01 14:34:36 +0000

    Day 1 - part one - read two list from file, order the lists of corresponding numbers, find the difference between corresponding pairs and sum the difference.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# avent-of-code-2024
+## Advent of Code 2024
+
+OK. It's time for [Advent of Code 2024](https://adventofcode.com/2024/). From my quick look at the 2023 puzzles I can't see me getting through all 24.
+But let's give it a go! I'm still keen to get a taste of different languages and frameworks in the process.
+I'll be looking for succinct code over performant cod and not worrying too much about error handling unless relevant to the puzzle.
+
+### Calendar
+
+1. ["Historian Hysteria"](https://adventofcode.com/2024/day/1) - I'm going to look at doing this in .NET. I use .NET day-to-day, but on Windows. I'm setting this up on Linux and using VS Code as my IDE to see what that experience is like.
---
+++ day1/Program.cs
@@ -0,0 +1,48 @@
+﻿
+// Day 1 - Part 1 asks us to order two lists of corresponding numbers, find the difference between corresponding pairs and sum the difference.
+var lists = ReadInputLists("input.txt");
+
+var leftList = lists.Item1.Order().ToArray();
+var rightList = lists.Item2.Order().ToArray();
+
+if (leftList.Length > rightList.Length)
+    throw new Exception("Lists were not of equal length");
+
+var sum = 0;
+
+for (var i = 0; i < leftList.Length; i++) {
+    Console.WriteLine($"i: {leftList[i]}, {rightList[i]}");
+    sum += Math.Abs(leftList[i] - rightList[i]);
+}
+
+Console.WriteLine($"Answer Is: {sum}");
+
+Console.ReadLine();
+
+
+// Parses the file at the specified filePath. Expects to find sets of lines containing two integers separated by whitespace
+// Returns a tupe containnig a list of the left-most and a list of the right-most integers
+static (List<int>, List<int>) ReadInputLists(string filePath)
+{
+    string? line;
+    var reader = new StreamReader(filePath);
+
+    line = reader.ReadLine();
+
+    var leftList = new List<int>();
+    var rightList = new List<int>();
+
+    while (line != null)
+    {
+        var parts = line.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length != 2) {
+            throw new Exception($"There were not exactly 2 parts in the line: {line}");
+        }
+        leftList.Add(int.Parse(parts[0]));
+        rightList.Add(int.Parse(parts[1]));
+        line = reader.ReadLine();
+    }
+    reader.Close();
+
+    return (leftList, rightList);
+}
