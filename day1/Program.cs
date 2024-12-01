d93a647e2228d66675352ef21a92d59e76917f5c
velmanatov <velmanatov@hotmail.com>
2024-12-01 15:29:55 +0000

    Day 1 - Part 2 - sum the number from the left list multiplied by how many times it appears in the right list

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,21 +1,35 @@
 ﻿
-// Day 1 - Part 1 asks us to order two lists of corresponding numbers, find the difference between corresponding pairs and sum the difference.
+// Day 1
+// Part 1 asks us to order two lists of corresponding numbers, find the difference between corresponding pairs and sum the difference.
+// Part 2 asks us to sum the number from the left list multiplied by how many times it appears in the right list
 var lists = ReadInputLists("input.txt");
 
+// Order the lists to simplify part 1 in particular
 var leftList = lists.Item1.Order().ToArray();
 var rightList = lists.Item2.Order().ToArray();
 
 if (leftList.Length > rightList.Length)
     throw new Exception("Lists were not of equal length");
 
-var sum = 0;
+var answerPart1 = 0;
+var answerPart2 = 0;
 
+// Given that we've ordered the lists, it is efficient to just iterate through the right hand list and do the counts of occurrences up front
+// numberCounts is a Dictionary mapping numbers found in right list to counts of those same numbers
+var numberCounts = CountOccurrencesInList(rightList);
+
+// Iterate through the left list, calculating the answer to both parts of the puzzle as we go
 for (var i = 0; i < leftList.Length; i++) {
-    Console.WriteLine($"i: {leftList[i]}, {rightList[i]}");
-    sum += Math.Abs(leftList[i] - rightList[i]);
+    var leftNum = leftList[i];
+    answerPart1 += Math.Abs(leftNum - rightList[i]);
+
+    if (numberCounts.TryGetValue(leftNum, out var rightNumCount)) {
+        answerPart2 += leftNum * rightNumCount;
+    }
 }
 
-Console.WriteLine($"Answer Is: {sum}");
+Console.WriteLine($"Answer to Part 1 Is: {answerPart1}");
+Console.WriteLine($"Answer to Part 2 Is: {answerPart2}");
 
 Console.ReadLine();
 
@@ -45,4 +59,35 @@ static (List<int>, List<int>) ReadInputLists(string filePath)
     reader.Close();
 
     return (leftList, rightList);
+}
+
+// Accepts an ordered list and iterates through it counting occurrences of each number found
+// Returns a dictionary mapping the number found to the count of that number
+static Dictionary<int, int> CountOccurrencesInList(int[] list) {
+    var numberCounts = new Dictionary<int, int>();
+
+    int? numBeingCounted = null;
+    var numCount = 0;
+
+    for (var i = 0; i < list.Length; i++) {
+        var num = list[i];
+        if (num != numBeingCounted) {
+            // When we find a new num...
+            if (numBeingCounted != null) {
+                // Store the count of the number we were counting if there is one...
+                numberCounts.Add(numBeingCounted.Value, numCount);
+                // Reset count
+                numCount = 0;
+
+            }
+            // Deals with last number in list occurring only once
+            if (i == list.Length - 1) {
+                numberCounts.Add(num, 1);
+            }
+        }
+        numCount ++;
+        numBeingCounted = num;
+    }
+
+    return numberCounts;
 }
