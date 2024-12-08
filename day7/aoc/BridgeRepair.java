1a3a1210175ea0926b36b2342f4179ba5faf9fa1
velmanatov <velmanatov@hotmail.com>
2024-12-08 01:31:42 +0000

    Day 7 - Part 1 in Java. Find all combinations of applying + and * operands and see if they give the required value

---
+++ day7/aoc/BridgeRepair.java
@@ -0,0 +1,86 @@
+package day7.aoc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BridgeRepair {
+
+    public static void main(String[] args) {
+        try {
+            Long answer1 = 0l;
+            File file = new File("./day7/input.txt");
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                CalibrationEquation equation = GetEquation(line);
+                if (equation.CanBeMadeTrue()) {
+                    answer1 += equation.testValue;
+                }
+             }
+             System.out.println("Answer 1: " + answer1);
+             reader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+    }
+
+    private static CalibrationEquation GetEquation(String line) {
+        // Sticking with two different ways of extracting the integers for no particular reason!
+        String[] parts = line.split(":");
+        Pattern numberRegex = Pattern.compile("\\d+");
+        Matcher matchTestValue = numberRegex.matcher(parts[0]);
+        matchTestValue.find();
+        
+        CalibrationEquation equation = new CalibrationEquation();
+        equation.testValue = Long.parseLong(matchTestValue.group());
+        List<Long> operands = new ArrayList<>();
+        Scanner scanner = new Scanner(parts[1]);
+        while (scanner.hasNextLong()) {
+            operands.add(scanner.nextLong());
+        }
+        scanner.close();
+
+        equation.operands = operands;
+        return equation;
+    }
+}
+
+class CalibrationEquation {
+    public Long testValue;
+    public List<Long> operands;
+
+    public boolean CanBeMadeTrue() {
+        // get the first set of possible results from applying all operands to the 1st and 2nd number
+        List<Long> results = ApplyAllOperations(List.of(operands.get(0)), operands.get(1));
+
+        // note that we are finidng all combinations. it would be more efficient to to depth first search and stop when we find any that give the correct answer
+        for(Integer i = 2; i < operands.size(); i ++) {
+            results = ApplyAllOperations(results, operands.get(i));
+        }
+
+        return results.contains(testValue);
+    }
+
+    private List<Long> ApplyAllOperations(List<Long> interimValues, Long value) {
+        List<Long> results = new ArrayList<Long>();
+
+        for (Long a: interimValues)
+        {
+            results.add(a + value);
+            results.add(a * value);
+        }
+        return results;
+    }
+
+    public String toString() {
+        return testValue.toString() + ": " + operands.toString();
+    }
+} 
