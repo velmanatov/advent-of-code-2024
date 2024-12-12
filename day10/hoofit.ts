6c9d9f73d7503946f87b69a1ca17bd4eb0b9414a
velmanatov <velmanatov@hotmail.com>
2024-12-12 00:11:39 +0000

    Day 10 - Part 2 - OK this was really quick given I'd basicaly already implemented finding every single trail from each start point.

--- day10/hoofit.ts
+++ day10/hoofit.ts
@@ -24,18 +24,22 @@ const mapHeight = topographicMap.length;
 
 // find all valid trails from each trailhead then find and count the unique end points and add to the answer for part 1
 let answer1: number = 0;
+let answer2: number = 0;
 for (let trailhead of trailheads) {
     const validTrails = findTrailsFrom(trailhead);
+    answer2 += validTrails.length;
     // unfortunately I've used tuples for my coords and [x, y] !== [x, y] so using toString() as a hack so the set is unqiue
     const uniqueTrailEnds = [...new Set(validTrails.map(trail => trail.at(-1).toString()))];
     answer1 += uniqueTrailEnds.length;
 }
 
 console.log('Answer 1: %d', answer1)
+console.log('Answer 2: %d', answer2)
 
 function findTrailsFrom(startPoint: [number, number]) : [number, number][][] {
 
     let validSubtrails: [number, number][][] = [[[startPoint[0], startPoint[1]]]];
+
     for (let height = 1; height <= 9; height ++) {
         validSubtrails = validTrailsNextStep(validSubtrails, height)
     }
