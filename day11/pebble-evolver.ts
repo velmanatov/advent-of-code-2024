f1ec294845de361abeac8634ed086065ca2d2799
velmanatov <velmanatov@hotmail.com>
2024-12-13 15:12:21 +0000

    Day 11 - Part 2 - our Part 1 solution doesn;t scale so I've written a recursive version. This is ridiculously slow but pushing for posterirty.

--- day11/pebble-evolver.ts
+++ day11/pebble-evolver.ts
@@ -1,5 +1,7 @@
 export class PebbleEvolver {
 
+    // This takes one pebble and applies a single blink, returning an array of evolved pebbles.
+    // Used in Part 1 solution, but does not scale to Part
     static blink(pebble: number) : number[] {
         if (pebble === 0) {
             return [1];
@@ -12,4 +14,26 @@ export class PebbleEvolver {
         }
     }
 
+    // Took so much help from reddit. Can't imgaine how long it would have taken to come up with this exact thing myself.
+    // but we can solve this recursively for Part 2. The above approach, maintaining an actual full expanded array of pebbles, does not scale!
+    // We only need the count for our answer and we can observe that the answer we get for each pebble is basically independent of the other pebbles
+    static countPebblesAfterBlinks(pebble: number, nBlinks: number): number {
+        // If we need zero more blinks we know we just need to add one more (the pebble under consideration)
+        if (nBlinks === 0)
+            return 1;
+        if (pebble === 0)
+            // For rule one we just need to apply our recursive count function again one a pebble with value 1
+            return PebbleEvolver.countPebblesAfterBlinks(1, nBlinks-1);
+        else {
+            const pebbleStr: string = '' + pebble;
+            if (pebbleStr.length % 2 === 0) {
+                // For rule 2 we can apply our count function independently and just add the two results
+                return PebbleEvolver.countPebblesAfterBlinks(+pebbleStr.substring(0, pebbleStr.length/2), nBlinks-1) +
+                    PebbleEvolver.countPebblesAfterBlinks(+pebbleStr.substring(pebbleStr.length/2), nBlinks-1);
+            }
+            // For rule 3 - like rule 1 - we can just apply the count function to the pebble with the new number.
+            // Preumably this is where we coudl optimise further?
+            return PebbleEvolver.countPebblesAfterBlinks(pebble * 2024, nBlinks-1);            
+        }     
+    }
 }
--- day11/pebbles.ts
+++ day11/pebbles.ts
@@ -3,10 +3,19 @@ import { PebbleEvolver } from './pebble-evolver';
 
 const fileData: string = fs.readFileSync('input.txt', 'utf-8');
 let pebbles: number[] = fileData.split(/\s+/).map(str => +str);
+let pebbles2: number[] = Object.assign([], pebbles);
 
 
-for (let i =0; i< 25; i++) {
+for (let i = 0; i< 25; i++) {
     pebbles = pebbles.flatMap(p => PebbleEvolver.blink(p));
 }
 
 console.log('Answer 1: %d', pebbles.length);
+
+let answer2: number = 0;
+for (const pebble of pebbles2) {
+
+    answer2 += PebbleEvolver.countPebblesAfterBlinks(pebble, 75);
+    console.log('pebble %d - answer now %d', pebble, answer2);
+}
+console.log('Answer 2: %d', answer2);
