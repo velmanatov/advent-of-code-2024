33f1ce9b0c67cfa8ab6f11849358dbdd19fa6596
velmanatov <velmanatov@hotmail.com>
2024-12-13 15:56:55 +0000

    Day 11 - Part 2 - cache answer for given sub branches of the recursion (i.e. one per nBlinks, pebble vaue) to vastly improve performance by not repeating lengthy recursions we already know the answer to.

--- day11/pebble-evolver.ts
+++ day11/pebble-evolver.ts
@@ -14,6 +14,10 @@ export class PebbleEvolver {
         }
     }
 
+    // For Part 2 we will keep a global track of what a given number of blinks results in for a pebbel value
+    // as the answer will always be the same
+    private static pebbleCounts: { [key: string]: number } = {};
+
     // Took so much help from reddit. Can't imgaine how long it would have taken to come up with this exact thing myself.
     // but we can solve this recursively for Part 2. The above approach, maintaining an actual full expanded array of pebbles, does not scale!
     // We only need the count for our answer and we can observe that the answer we get for each pebble is basically independent of the other pebbles
@@ -21,19 +25,34 @@ export class PebbleEvolver {
         // If we need zero more blinks we know we just need to add one more (the pebble under consideration)
         if (nBlinks === 0)
             return 1;
-        if (pebble === 0)
-            // For rule one we just need to apply our recursive count function again one a pebble with value 1
-            return PebbleEvolver.countPebblesAfterBlinks(1, nBlinks-1);
-        else {
+
+        if (pebble === 0) {
+            return PebbleEvolver.getCount(1, nBlinks - 1);
+        } else {
             const pebbleStr: string = '' + pebble;
             if (pebbleStr.length % 2 === 0) {
                 // For rule 2 we can apply our count function independently and just add the two results
-                return PebbleEvolver.countPebblesAfterBlinks(+pebbleStr.substring(0, pebbleStr.length/2), nBlinks-1) +
-                    PebbleEvolver.countPebblesAfterBlinks(+pebbleStr.substring(pebbleStr.length/2), nBlinks-1);
+                return PebbleEvolver.getCount(+pebbleStr.substring(0, pebbleStr.length/2), nBlinks-1) +
+                    PebbleEvolver.getCount(+pebbleStr.substring(pebbleStr.length/2), nBlinks-1);
             }
             // For rule 3 - like rule 1 - we can just apply the count function to the pebble with the new number.
             // Preumably this is where we coudl optimise further?
-            return PebbleEvolver.countPebblesAfterBlinks(pebble * 2024, nBlinks-1);            
+            return PebbleEvolver.getCount(pebble * 2024, nBlinks-1);            
         }     
     }
+
+    // Either get a cached count or otherwise actually work out the count recursively for the current number of blinnks and pebble number
+    private static getCount(pebble: number, nBlinks: number, )  {
+        // couldn't get index type containing indexed type right so just use a string key instead
+        let lookupKey: string = '' + nBlinks + '.' + pebble;
+
+        let count: number = PebbleEvolver.pebbleCounts[lookupKey];
+        if (count)
+            return count;
+        
+        // For rule one we just need to apply our recursive count function again one a pebble with value 1
+        count = PebbleEvolver.countPebblesAfterBlinks(pebble, nBlinks);
+        PebbleEvolver.pebbleCounts[lookupKey] = count;
+        return count;
+    } 
 }
--- day11/pebbles.ts
+++ day11/pebbles.ts
@@ -3,8 +3,8 @@ import { PebbleEvolver } from './pebble-evolver';
 
 const fileData: string = fs.readFileSync('input.txt', 'utf-8');
 let pebbles: number[] = fileData.split(/\s+/).map(str => +str);
+// take a copy of the parsed input for use in Part 2 solution
 let pebbles2: number[] = Object.assign([], pebbles);
-
 
 for (let i = 0; i< 25; i++) {
     pebbles = pebbles.flatMap(p => PebbleEvolver.blink(p));
@@ -14,8 +14,6 @@ console.log('Answer 1: %d', pebbles.length);
 
 let answer2: number = 0;
 for (const pebble of pebbles2) {
-
     answer2 += PebbleEvolver.countPebblesAfterBlinks(pebble, 75);
-    console.log('pebble %d - answer now %d', pebble, answer2);
 }
 console.log('Answer 2: %d', answer2);
